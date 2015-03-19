0f54014f0dfcd19f12e9e70c70b12506ebf83fca
Andris Raugulis <araugulis@evolutiongaming.com>
2015-03-19 12:12:51 +0200

    First two coding challenges.

---
+++ coding.ch14.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import os, sys, re, datetime
+import requests, hashlib
+import lxml.html
+
+VERBOSE = True
+URL = 'http://ringzer0team.com'
+START_TIME = datetime.datetime.now()
+
+def _tsout(*objs):
+	prefix = '[{0}]'.format(datetime.datetime.now() - START_TIME)
+	print(prefix, *objs, file=sys.stdout)
+
+def _out(*objs):
+	print(*objs, file=sys.stdout)
+
+def get_auth():
+	username, password = None, None
+	if not sys.stdin.isatty():
+		lines = sys.stdin.readlines()
+		l = len(lines)
+		if l == 1:
+			p = lines[0].split(' ')
+			if len(p) > 1: 
+				username = p[0].strip()
+				password = p[1].strip()
+		elif l > 1:
+			username = lines[0].strip()
+			password = lines[1].strip()
+	else:
+		username = raw_input('username: ').strip()
+		password = raw_input('password: ').strip()
+	if not username or not password:
+		_out('error: wrong auth input')
+		sys.exit(1)
+	return (username, password)
+
+def get_msg(html):
+	doc = lxml.html.document_fromstring(html)
+	wrapper = doc.xpath('//div[@class="challenge-wrapper"]')[0]
+	text = wrapper.text_content()
+	state, title, msg = 0, [], []
+	for s in text.splitlines():
+		s = s.strip()
+		if not s: continue
+		if state == 0 and s == '----- BEGIN MESSAGE -----':
+			state = 1
+			continue
+		elif state == 1 and s == '----- END MESSAGE -----':
+			state = 2
+			continue
+		if state == 0:
+			title.append(s)
+		elif state == 1:
+			msg.append(s)
+	return (os.linesep.join(title), os.linesep.join(msg))
+
+def get_response(html):
+	doc = lxml.html.document_fromstring(html)
+	wrapper = doc.xpath('//div[@class="challenge-wrapper"]')[0]
+	xalert = wrapper.xpath('./div[contains(@class, "alert")]')[0]
+	return xalert.text_content().strip()
+
+def ch14(username, password, verbose = False):
+	if verbose: 
+		_tsout('logging')
+	s = requests.Session()
+	payload = {'username':username, 'password':password}
+	r = s.post(URL + '/login', data=payload)
+	
+	if verbose: 
+		_tsout('reading challenge')
+	r = s.get(URL + '/challenges/14')
+	title, msg = get_msg(r.text)
+	if verbose:
+		_tsout('title: ' + title)
+		_tsout('message: ' + msg)
+	
+	if verbose:
+		_tsout('solving')
+	mx = re.search(r'using ([a-z0-9]+) algorithm', title)
+	algorithm = mx.group(1)
+	h = hashlib.new(algorithm)
+	data = ''.join(chr(int(msg[i:i+8], 2)) for i in xrange(0, len(msg), 8))
+	h.update(data)
+	msg_hash = h.hexdigest()
+	if verbose:
+		_tsout('solved: ' + msg_hash)
+	
+	if verbose:
+		_tsout('submitting solution')
+	r = s.get(URL + '/challenges/14/' + msg_hash)
+	response = get_response(r.text)
+	if verbose:
+		_tsout('response: ' + response)
+	else:
+		_out(response)
+
+
+if __name__ == '__main__':
+	username, password = get_auth()
+	ch14(username, password, VERBOSE)
+	sys.exit(0);
