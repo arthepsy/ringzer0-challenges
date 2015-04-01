69b7bfbc5e0e24ed3556847e8c3bc464e395ac11
Andris Raugulis <araugulis@evolutiongaming.com>
2015-04-02 02:49:02 +0300

    17th forensics challenge.

---
+++ forensics.ch163.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+
+PACKETS_FILE = 'ch163.packets.txt'
+
+def ch163(fp):
+	#keys = []
+	flag = [20 for i in range(37)]
+	with open(fp, 'r') as f:
+		for line in f:
+			if not 'User id:1' in line or not 'flag AS' in line:
+				continue
+			p = line.strip().split(' ')
+			found = p[-2:-1][0] == 'was'
+			if not found:
+				continue
+			last = p[-3:][0]
+			last, c = last.split('>')
+			c, n = int(c), int(last.split(',')[2]) - 1
+			if found and flag[n] < c:
+				flag[n] = c
+	print ''.join(chr(c + 1) for c in flag)
+
+if __name__ == '__main__':
+	ch163(PACKETS_FILE)
